ff5abebf11cf295579ef83f732faa9d1c2547bfb
Ferdous Hossain Fahim <ferdbugs@gmail.com>
2019-06-14 17:24:31 +0800

    Code now Loops in Search Page

--- import requests.py
+++ import requests.py
@@ -9,81 +9,83 @@ from selenium.webdriver.common.keys import Keys
 import csv
 
 
-UnfID = []
 
-def login(UnfID):
+def login(IDlist):
     Msg = "Approved"
     Msg2 = "Mammogram"
     Msg3 = "You have yet to certify that the report is completed,"
     Msg4 = "Processing"
     Msg5 = "No users in this list"
-    ID = ''.join(e for e in UnfID if e.isalnum())
     driver = webdriver.Chrome()
     driver.get ("https://sehat.perkeso.gov.my/v2/")
-    driver.find_element_by_id("modlgn-username").send_keys("DCDU2")
-    driver.find_element_by_id ("modlgn-passwd").send_keys("BPDCDU2")
+    driver.find_element_by_id("modlgn-username").send_keys("BPDCKLANG")
+    driver.find_element_by_id ("modlgn-passwd").send_keys("BPDCKLANG1")
     driver.find_element_by_name("Submit").click()
     driver.find_element_by_class_name("item-711").click()
     driver.find_element_by_xpath("//*[@id='content']/div[2]/div[2]/table/tbody/tr[2]/td[5]/a").click()
-    driver.find_element_by_id("cb_ic").send_keys(ID)
-    driver.find_element_by_xpath("//*[@id='adminForm']/div[3]/div[2]/div[2]/div/input").click()
-    content = driver.page_source.encode('utf-8').strip()
-    soup = BeautifulSoup(content, "html.parser")
-    status = soup.find("div",id="cbUserTable")
-    div = soup.prettify("utf-8")
-    print((str(div)).count(Msg2))
-    if Msg in str(div):
-        with open(r'Status.csv', 'a') as f:
-            writer = csv.writer(f,lineterminator = '\n')
-            row =[ID,Msg,"GREEN"]
-            writer.writerow(row)
-            f.close
-    elif Msg5 in str(div):
-        with open(r'Status.csv', 'a') as f:
-            writer = csv.writer(f,lineterminator = '\n')
-            row =[ID,Msg5]
-            writer.writerow(row)
-            f.close
-    elif Msg3 in str(div):
-        with open(r'Status.csv', 'a') as f:
-            writer = csv.writer(f,lineterminator = '\n')
-            row =[ID,Msg3,"BLUE"]
-            writer.writerow(row)
-            f.close
-    elif Msg4 in str(div):
-        with open(r'Status.csv', 'a') as f:
-            writer = csv.writer(f,lineterminator = '\n')
-            row =[ID,Msg4,"ORANGE"]
-            writer.writerow(row)
-            f.close
-    else:
-        with open(r'Status.csv', 'a') as f:
-            writer = csv.writer(f,lineterminator = '\n')
-            row =[ID,"Not Approved"]
-            writer.writerow(row)
-            f.close
-    if (str(div)).count(Msg2)>1:
-        if Msg3 in str(div) or Msg4 in str(div):
+    for ID in IDlist:
+        driver.get ("https://sehat.perkeso.gov.my/v2/component/comprofiler/userslist/9-my-patients-search-patients/search.html")
+        driver.find_element_by_id("cb_ic").send_keys(ID)
+        driver.find_element_by_xpath("//*[@id='adminForm']/div[3]/div[2]/div[2]/div/input").click()
+        content = driver.page_source.encode('utf-8').strip()
+        soup = BeautifulSoup(content, "html.parser")
+        status = soup.find("div",id="cbUserTable")
+        div = soup.prettify("utf-8")
+        print((str(div)).count(Msg2))
+        if Msg in str(div):
             with open(r'Status.csv', 'a') as f:
                 writer = csv.writer(f,lineterminator = '\n')
-                row =[ID,"One Test Not Approved"]
+                row =[ID,Msg,"GREEN"]
+                writer.writerow(row)
+                f.close
+        elif Msg5 in str(div):
+            with open(r'Status.csv', 'a') as f:
+                writer = csv.writer(f,lineterminator = '\n')
+                row =[ID,Msg5]
+                writer.writerow(row)
+                f.close
+        elif Msg3 in str(div):
+            with open(r'Status.csv', 'a') as f:
+                writer = csv.writer(f,lineterminator = '\n')
+                row =[ID,Msg3,"BLUE"]
+                writer.writerow(row)
+                f.close
+        elif Msg4 in str(div):
+            with open(r'Status.csv', 'a') as f:
+                writer = csv.writer(f,lineterminator = '\n')
+                row =[ID,Msg4,"ORANGE"]
                 writer.writerow(row)
                 f.close
         else:
             with open(r'Status.csv', 'a') as f:
                 writer = csv.writer(f,lineterminator = '\n')
-                row =[ID,"Both Tests Approved"]
+                row =[ID,"Not Approved"]
                 writer.writerow(row)
                 f.close
-        
+        if (str(div)).count(Msg2)>1:
+            if Msg3 in str(div) or Msg4 in str(div):
+                with open(r'Status.csv', 'a') as f:
+                    writer = csv.writer(f,lineterminator = '\n')
+                    row =[ID,"One Test Not Approved"]
+                    writer.writerow(row)
+                    f.close
+            else:
+                with open(r'Status.csv', 'a') as f:
+                    writer = csv.writer(f,lineterminator = '\n')
+                    row =[ID,"Both Tests Approved"]
+                    writer.writerow(row)
+                    f.close       
     driver.close()
     driver.quit()
-
+UnfID = []
+IDlist = []
 df = pd.read_csv("ICs.csv")
 for idx in range(0,len(df.index)):         
     row = df.iloc[idx]
-    UnfID = row['IDs'] 
-    login(UnfID)
+    UnfID.append(row['IDs'])
+for IDx in UnfID:
+    IDlist.append(''.join(e for e in IDx if e.isalnum()))
+login(IDlist)
     
 #def webcrawler():                  
     # autologin = {
